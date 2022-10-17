f04f4a46b4f659964fb55948f89f5fca2eb5aa48
ex4ce <lobazhevichpavel@gmail.com>
2022-10-17 22:21:25 +0300

    Added final parametrization task

--- Code/final_pytest_parametrization/test_items.py
+++ Code/final_pytest_parametrization/test_items.py
@@ -3,7 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_button(browser):
+def test_find_card_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     # time.sleep(5)
     button = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
